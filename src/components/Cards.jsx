9f43a2a51314c2c2444a32154aad7e95a334fed4
Fred Di Pasqua <freddipassca@gmail.com>
2020-09-02 23:11:33 -0500

    Primera card base creada

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 import '../assets/styles/components/Cards.scss'
 
 const Card = () => (
-    <div></div>
+    <div className="container">
+        <div className="card">
+            <div className="imgBx">
+                <div className="name" >
+                    <h1>Arumi Mendoza Castellanos</h1>
+                </div>
+            </div>
+            <div className="content">
+                <h2>Lic. Antropologia fisica</h2>
+                <p>Becaria en el laboratorio de fisiologia
+                    y genetica con promedio de 9
+                </p>
+            </div>
+        </div>
+    </div>
 );
 
 export default Card;
