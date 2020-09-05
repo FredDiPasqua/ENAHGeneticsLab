c32d113f7cd0ef965aa2ab62cdbcb5ece77b77d7
Fred Di Pasqua <freddipassca@gmail.com>
2020-09-05 13:35:09 -0500

    Se modificó el menu, title y colores

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,17 +9,24 @@ const Header = () => {
         <>
             <div className="header" >
                 <img className="logo" src={logo} alt=""/>
-                <h1 className="title" >Laboratorio de Genética Molecular</h1>
+                <div className="title">
+                    <h1>Laboratorio de Genética Molecular</h1>
+                    <p>Licenciatura en Antropología Física   |   Escuela Nacional de Antropología e Historia</p>
+
+                </div>
                 <img className="enah-logo" src={logoEnah} alt=""/>
 
             </div>
             <div className="menu" >
                 <NavLink className="links" to="/">Inicio</NavLink>
+                <hr/>
                 <NavLink className="links" to="/about">Acerca de</NavLink>
+                <hr/>
                 <NavLink className="links" to="/research">Líneas de Investigación</NavLink>
+                <hr/>
                 <NavLink className="links" to="/members">Miembros</NavLink>
+                <hr/>
                 <NavLink className="links" to="/proyect">Proyecto Ética de Investigación</NavLink>
-
             </div>
         </>
 
