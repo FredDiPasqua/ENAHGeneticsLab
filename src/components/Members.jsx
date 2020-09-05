0f54203e5440d47a1101557adb03f200b7d6b73e
Fred Di Pasqua <freddipassca@gmail.com>
2020-09-05 14:15:20 -0500

    se trabajó el about

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -7,64 +7,7 @@ const Members = () => (
     <div className="members" >
         <h1>Miembros</h1>
         <div className="jerarquias">
-            <Card
-                class="card_chiefs" 
-                nombre="  A.F. Víctor Acuña Alonzo  "
-                puesto="Responsable Académico"
-                labor="Profesor Investigador, INAH" 
-            />
-            <Card
-                class="card_chiefs" 
-                nombre="A.F. Miguel Ángel Contreras Sieck"
-                puesto="Responsable Técnico"
-                labor="Profesor H-S-M" 
-            />
-        </div>
-        <div className="jerarquias">
-            <Card
-                class="card_colaboradores"
-                nombre="A.F. Paola Everardo Martínez"
-                puesto="..."
-                labor="..."
-            />
-            <Card
-                class="card_colaboradores"
-                nombre="A.F. Gastón Macín Pérez"
-                puesto="..."
-                labor="..."
-            />
-            <Card
-                class="card_colaboradores"
-                nombre="A.F. Mónica Ballesteros Romero"
-                puesto="..."
-                labor="..."
-            />
-        </div>
-        <div className="jerarquias">
-            <Card
-                class="card_becarios" 
-                nombre="Daniela Sandoval Morales"
-                puesto="Estudiante en Antropología Física"
-                labor="Becaria "
-            />
-            <Card
-                class="card_becarios" 
-                nombre="Claudia Quiroz Lopez"
-                puesto="Estudiante en Antropología Física"
-                labor="Becaria "
-            />
-            <Card
-                class="card_becarios" 
-                nombre="Alejandro Alvarado González"
-                puesto="Estudiante en Antropología Física"
-                labor="Becaria "
-            />
-            <Card
-                class="card_becarios" 
-                nombre="Arumi Mendoza Castellanos"
-                puesto="Estudiante en Antropología Física"
-                labor="Becaria "
-            />
+           
         </div>
     </div>
 );
